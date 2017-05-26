7fa3012b212e7693fc5866192e018e06a6aaa5b7
Farhad Ghorbani <farhad.ghorbani@gmail.com>
2017-05-26 15:43:39 -0400

    exampl.py modifies to give a CSV out

--- example.py
+++ example.py
@@ -1,15 +1,24 @@
 #! /Users/farhad/anaconda/bin/python
 
 import sqlite3
+import csv
 
 con = sqlite3.connect("survey.db")
 cur = con.cursor()
-cur.execute("SELECT * from Person;")
+cur.execute("SELECT max(reading), min(reading), sum(reading), avg(reading) from Survey;")
 results = cur.fetchall()
-#print(results)
 
+k = ['max','min','sum','avg']
 for i in results:
-    print(i)
+    for j in range(len(i)):
+        print(k[j],'=',i[j])
+
+#cur = con.cursor()
+cur.execute("SELECT * from Site;")
+results2 = cur.fetchall()
+out = csv.writer(open('data.csv','w'))
+for d in results2:
+    out.writerow(d)
 
 cur.close()
 con.close()
